678bbfcd306c401f37d0df47d01ac6573048ba8d
Rakesh Shrestha <rakeshshrestha3222@gmail.com>
2021-12-14 22:52:48 +0545

    login and register api with validation

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const bcryptjs = require('bcryptjs');
+const bcrypt = require('bcryptjs/dist/bcrypt');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please provide a username"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please providea email"],
+        unique: true,
+        match:[
+            /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/,
+            "Please provide a valid email"
+        ]
+    },
+    password: {
+        type: String,
+        required: [true, "Please add a password"],
+        minlength: 6,
+        select: false //password won't be send back - features
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date
+});
+
+//middleare of mongoose
+UserSchema.pre("save", async function(next){
+    if(!this.isModified("password")){
+        //won't rehash direct save(package= bcryptjs)
+        next()
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt)
+    next();
+})
+
+UserSchema.methods.matchPasswords = async function(password){
+    return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
